e9b56b9287d739cab774185dde5d8a5dbff0fd43
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2024-02-16 20:37:01 -0300

    add: Estructura para realizar el CRUD de las modelos

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,11 +2,13 @@ import { z } from 'zod';
 import { protectedProcedure, publicProcedure, router } from './trpc';
 import { TRPCError } from '@trpc/server';
 import { perfilRouter } from '@/server/routers/perfilRouter';
+import { modelosRouter } from './routers/modelosRouter';
 
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export const appRouter = router({
   perfil: perfilRouter,
+  modelos: modelosRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
