b2185e60162a613c09bc82ee8f422d2b358a39a8
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2024-02-18 01:14:08 -0300

    add: GET de las modelos

--- src/server/routers/modelosRouter.ts
+++ src/server/routers/modelosRouter.ts
@@ -3,5 +3,51 @@ import { TRPCError } from '@trpc/server';
 import { z } from 'zod';
 
 export const modelosRouter = router({
-    
+    getAll: publicProcedure.query(async ({ ctx }) => {
+        return await ctx.prisma.perfil.findMany();
+    }),
+    getById: publicProcedure.input(z.string().uuid()).query(async ({ input, ctx }) => {
+        return await ctx.prisma.perfil.findUnique({
+            where: {
+                id: input,
+            },
+        });
+    }),
+    getByEtiqueta: publicProcedure.input(z.string().array()).query(async ({ input, ctx }) => {
+        return await ctx.prisma.perfil.findMany({
+            where: {
+                etiquetas: {
+                    some: {
+                        nombre: { in: input },
+                    },
+                },
+            },
+            select: {
+                nombrePila: true,
+                etiquetas: true,
+            },
+        });
+    }),
+    getByGrupoEtiqueta: publicProcedure.input(z.string().array()).query(async ({ input, ctx }) => {
+        return await ctx.prisma.perfil.findMany({
+            where: {
+                etiquetas: {
+                    some: {
+                        grupo: {
+                            nombre: { in: input },
+                        },
+                    },
+                },
+            },
+            select: {
+                id: true,
+                nombrePila: true,
+                etiquetas: {
+                    select: {
+                        grupo: true,
+                    }
+                },
+            },
+        });
+    }),
 });
