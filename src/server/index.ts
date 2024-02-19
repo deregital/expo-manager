7d8e2c281a73b2d991fa4b5ab53ec61404bd786d
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2024-02-19 15:39:46 -0300

    add: estructura para realizar el CRUD de etiquetas

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,13 +2,15 @@ import { z } from 'zod';
 import { protectedProcedure, publicProcedure, router } from './trpc';
 import { TRPCError } from '@trpc/server';
 import { perfilRouter } from '@/server/routers/perfilRouter';
-import { modeloRouter } from './routers/modelosRouter';
+import { modeloRouter } from '@/server/routers/modelosRouter';
+import { etiquetaRouter } from '@/server/routers/etiquetaRouter';
 
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export const appRouter = router({
   perfil: perfilRouter,
   modelo: modeloRouter,
+  etiqueta: etiquetaRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
