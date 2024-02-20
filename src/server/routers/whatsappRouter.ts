40c2acd46dc2a7141ea477838b5b839a77f6f9d8
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2024-02-20 14:58:52 -0300

    add: estructura para las funciones de whatsapp

---
+++ src/server/routers/whatsappRouter.ts
@@ -0,0 +1,8 @@
+import { protectedProcedure, publicProcedure, router } from '@/server/trpc';
+import { TRPCError } from '@trpc/server';
+import { z } from 'zod';
+
+
+export const whatsappRouter = router({
+
+})
