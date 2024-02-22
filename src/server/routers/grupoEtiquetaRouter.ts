b2487cfb668518aa07fcaad756cfe3fb2058441e
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2024-02-22 16:09:23 -0300

    add: create grupo de etiquetas

--- src/server/routers/grupoEtiquetaRouter.ts
+++ src/server/routers/grupoEtiquetaRouter.ts
@@ -3,5 +3,17 @@ import { TRPCError } from '@trpc/server';
 import { z } from 'zod';
 
 export const grupoEtiquetaRouter = router({
-
+    create: protectedProcedure.input(z.object({
+        nombre: z.string(),
+        color: z.string().length(7).startsWith('#').toLowerCase(),
+        esExclusivo: z.boolean(),
+    })).mutation(async ({ input, ctx }) => {
+        return await ctx.prisma.etiquetaGrupo.create({
+            data: {
+                nombre: input.nombre,
+                color: input.color,
+                esExclusivo: input.esExclusivo,
+            }
+        });
+    }),
 })
