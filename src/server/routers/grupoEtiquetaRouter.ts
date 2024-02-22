dde8ffe5fb58744038a23eb5da913a68aea01a08
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2024-02-22 16:02:57 -0300

    add: estructura para realizar el CRUD de grupo de etiquetas

---
+++ src/server/routers/grupoEtiquetaRouter.ts
@@ -0,0 +1,7 @@
+import { protectedProcedure, publicProcedure, router } from '@/server/trpc';
+import { TRPCError } from '@trpc/server';
+import { z } from 'zod';
+
+export const grupoEtiquetaRouter = router({
+
+})
