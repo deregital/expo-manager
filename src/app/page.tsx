0eabe7d1f2767ec59122b0e33a864572175c345f
nicofishman <nicourifishman@gmail.com>
2024-02-22 00:48:11 -0300

    test: A ver si me lo arregla

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 import Greeting from '@/components/Greeting';
+import { useState } from 'react';
 
 export default function Home() {
+
+
+
+    
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Greeting />
