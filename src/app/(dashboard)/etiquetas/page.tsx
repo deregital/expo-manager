05b668734e3d831e4a064d04d175d79330146db6
nicofishman <nicourifishman@gmail.com>
2024-03-01 14:34:53 -0300

    add: Lista de etiquetas

--- src/app/(dashboard)/etiquetas/page.tsx
+++ src/app/(dashboard)/etiquetas/page.tsx
@@ -1,9 +1,28 @@
+'use client';
+import { Input } from '@/components/ui/input';
+import { useDebounceValue } from 'usehooks-ts';
 import React from 'react';
+import { trpc } from '@/lib/trpc';
+
+import EtiquetasList from '@/components/etiquetas/EtiquetasList';
 
 const EtiquetasPage = () => {
+  const [search, setSearch] = useDebounceValue('', 500);
+  const { data: grupos } = trpc.etiqueta.getByNombre.useQuery(search);
+
   return (
-    <div>
-      <p>EtiquetasPage</p>
+    <div className='p-4'>
+      <div className='flex justify-between'>
+        {/* div para botones de crear e input */}
+        <p>EtiquetasPage</p>
+        <Input
+          className='max-w-md'
+          placeholder='Buscar grupo o etiqueta'
+          //   value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+      <EtiquetasList grupos={grupos ?? []} />
     </div>
   );
 };
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,19 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function getTextColorByBg(bg: string) {
+  const color = bg.charAt(0) === '#' ? bg.substring(1, 7) : bg;
+  const r = parseInt(color.substring(0, 2), 16);
+  const g = parseInt(color.substring(2, 4), 16);
+  const b = parseInt(color.substring(4, 6), 16);
+  const uicolors = [r / 255, g / 255, b / 255];
+  const c = uicolors.map((col) => {
+    if (col <= 0.03928) {
+      return col / 12.92;
+    }
+    return Math.pow((col + 0.055) / 1.055, 2.4);
+  });
+  const L = 0.2126 * c[0] + 0.7152 * c[1] + 0.0722 * c[2];
+  return L > 0.179 ? 'black' : 'white';
+}
