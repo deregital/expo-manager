91ead1790f4cc6e6c0ad319e5ed802d5387ddbf9
nicofishman <nicourifishman@gmail.com>
2024-03-01 14:17:29 -0300

    feat: Tipo de RouterOutput para inferir los return

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,6 +3,7 @@ import { modeloRouter } from '@/server/routers/modelosRouter';
 import { etiquetaRouter } from '@/server/routers/etiquetaRouter';
 import { whatsappRouter } from '@/server/routers/whatsappRouter';
 import { grupoEtiquetaRouter } from '@/server/routers/grupoEtiquetaRouter';
+import { inferRouterOutputs } from '@trpc/server';
 
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
@@ -14,3 +15,4 @@ export const appRouter = router({
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
+export type RouterOutputs = inferRouterOutputs<AppRouter>;
