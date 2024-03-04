a2735a0024754d5ccae43298eadc442ca936e244
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2024-03-04 17:21:23 -0300

    feat: Mínimo 1 caracter en el nombre del grupo en el edit

--- src/server/routers/grupoEtiquetaRouter.ts
+++ src/server/routers/grupoEtiquetaRouter.ts
@@ -42,7 +42,7 @@ export const grupoEtiquetaRouter = router({
     .input(
       z.object({
         id: z.string().uuid(),
-        nombre: z.string().optional(),
+        nombre: z.string().min(1).optional(),
         color: z.string().length(7).startsWith('#').toLowerCase().optional(),
         esExclusivo: z.boolean().optional(),
       })
