0c7fe0fa6416f16734cb2870a085bdcf6ba22b30
nicofishman <nicourifishman@gmail.com>
2024-03-05 17:20:46 -0300

    feat: Cambios en UI login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -23,8 +23,6 @@ const LoginPage = () => {
     if (res?.status === 401) {
       setError('Credenciales inválidas');
     }
-
-    console.log(res);
   }
 
   const session = useSession();
@@ -34,30 +32,30 @@ const LoginPage = () => {
   }
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-white font-sans antialiased">
-      <fieldset className="w-full max-w-xl rounded-md border-2 border-slate-500/50 p-7 pb-4">
-        <legend className="text-center">
-          <h1 className="text-3xl font-bold text-slate-900">Iniciar Sesión</h1>
-        </legend>
-        <form action={handleLogin} className="flex flex-col gap-4">
+    <div className='flex min-h-screen flex-col items-center justify-center bg-white font-sans antialiased'>
+      <legend className='mb-4 text-center'>
+        <h1 className='text-3xl font-bold text-slate-900'>Iniciar Sesión</h1>
+      </legend>
+      <fieldset className='w-full max-w-[calc(100%-16px)] rounded-md border-2 border-slate-500/50 p-7 pb-4 lg:max-w-xl'>
+        <form action={handleLogin} className='flex flex-col gap-4'>
           <Input
-            className="bg-white"
-            type="text"
-            name="username"
-            id="username"
-            placeholder="Nombre de Usuario"
+            className='bg-white'
+            type='text'
+            name='username'
+            id='username'
+            placeholder='Nombre de Usuario'
           />
           <Input
-            className="bg-white"
-            type="password"
-            name="password"
-            id="password"
-            placeholder="Contraseña"
+            className='bg-white'
+            type='password'
+            name='password'
+            id='password'
+            placeholder='Contraseña'
           />
-          <Button type="submit">Log In</Button>
+          <Button type='submit'>Log In</Button>
         </form>
         {error && (
-          <p className="mt-2 text-sm font-bold text-red-500">{error}</p>
+          <p className='mt-2 text-sm font-bold text-red-500'>{error}</p>
         )}
       </fieldset>
     </div>
