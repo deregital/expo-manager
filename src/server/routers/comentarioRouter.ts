aa9e1a127811e77ced7fab62dce63951da77c7ef
Alan Babor <baboralanoroynegro@gmail.com>
2024-03-07 17:02:27 -0300

    add: CREATE del router de comentario

--- src/server/routers/comentarioRouter.ts
+++ src/server/routers/comentarioRouter.ts
@@ -1,7 +1,36 @@
 import { protectedProcedure, publicProcedure, router } from '@/server/trpc';
+import { TRPCError } from '@trpc/server';
 import { z } from 'zod';
 
 export const comentarioRouter = router({
-    
+    create: protectedProcedure
+    .input(
+      z.object({
+        contenido: z.string().min(1),
+        perfilId: z.string(),
+      })
+    )
+    .mutation(async ({ input, ctx }) => {
 
-})
+        const userId = ctx.session?.user?.id; 
+        if (!userId) {
+            throw new TRPCError({ code: 'UNAUTHORIZED', message: 'Usuario no autenticado' }); 
+          }
+        
+
+        return await ctx.prisma.comentario.create({
+          data: {
+            contenido: input.contenido,
+            perfilId: input.perfilId,  
+            creadoPor: userId, 
+          },
+        });
+  
+      }),
+  });
+
+
+
+
+
+
