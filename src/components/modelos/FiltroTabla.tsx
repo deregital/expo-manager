c221998e83bd9c9317857a5ccf4931a9c17338e9
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2024-03-07 16:06:33 -0300

    feat: Orden de los filtros

--- src/components/modelos/FiltroTabla.tsx
+++ src/components/modelos/FiltroTabla.tsx
@@ -3,6 +3,8 @@ import { trpc } from '@/lib/trpc';
 import { Input } from '../ui/input';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import ComboBoxModelos from './ComboBoxModelos';
+import EtiquetaComboBoxModelos from './EtiquetaComboBox';
+import { SearchIcon } from 'lucide-react';
 
 export default function FiltroTabla() {
   const searchParams = new URLSearchParams(useSearchParams());
@@ -10,15 +12,23 @@ export default function FiltroTabla() {
   const router = useRouter();
   const { data: Grupos } = trpc.grupoEtiqueta.getAll.useQuery();
   return (
-    <>
-      <ComboBoxModelos data={Grupos ?? []} />
-      <Input
-        placeholder='Buscar por nombre o id legible'
-        onChange={(e) => {
-          searchParams.set('nombre', e.target.value);
-          router.push(`${pathname}?${searchParams.toString()}`);
-        }}
-      />
-    </>
+    <div className='flex items-center justify-between'>
+      <div className='flex items-center justify-center gap-x-5'>
+        <ComboBoxModelos data={Grupos ?? []} />
+        <EtiquetaComboBoxModelos />
+      </div>
+      <div className='relative w-full max-w-[300px] pr-5'>
+        <span className='pointer-events-none absolute inset-y-0 right-5 flex cursor-crosshair items-center pr-3 text-muted-foreground'>
+          <SearchIcon className='h-5 w-5' />
+        </span>
+        <Input
+          placeholder='Buscar por nombre o id legible'
+          onChange={(e) => {
+            searchParams.set('nombre', e.target.value);
+            router.push(`${pathname}?${searchParams.toString()}`);
+          }}
+        />
+      </div>
+    </div>
   );
 }
