8b73fcdbfbb8fba08f52595f60309f4478ea4759
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2024-03-07 17:58:45 -0300

    feat: Sacar componente de Tabla y mostrar modelos filtro

--- src/app/(dashboard)/modelos/page.tsx
+++ src/app/(dashboard)/modelos/page.tsx
@@ -1,13 +1,30 @@
+'use client';
 import FiltroTabla from '@/components/modelos/FiltroTabla';
-import ModelosTabla from '@/components/modelos/ModelosTabla';
+import { trpc } from '@/lib/trpc';
+import { useSearchParams } from 'next/navigation';
 import React from 'react';
 
 const ModelosPage = () => {
+  const searchParams = new URLSearchParams(useSearchParams());
+  const { data: modelos } = trpc.modelo.getByFiltro.useQuery(getParams());
+  function getParams() {
+    return {
+      nombre: searchParams.get('nombre') ?? undefined,
+      etiquetaId: searchParams.get('etiqueta') ?? undefined,
+      grupoId: searchParams.get('grupoId') ?? undefined,
+    };
+  }
   return (
     <div>
       <p>ModelosPage</p>
       <FiltroTabla />
-      <ModelosTabla />
+      {modelos
+        ? modelos.map((modelo) => (
+            <div key={modelo.id}>
+              <p>{modelo.nombreCompleto}</p>
+            </div>
+          ))
+        : 'Cargando...'}
     </div>
   );
 };
