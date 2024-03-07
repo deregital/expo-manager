0fd87f80029a441fef4f19a44e773604defcd896
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2024-03-07 15:10:33 -0300

    add: Nombre a los params para el filtro

--- src/components/modelos/FiltroTabla.tsx
+++ src/components/modelos/FiltroTabla.tsx
@@ -1,21 +1,23 @@
 'use client';
 import { trpc } from '@/lib/trpc';
 import { Input } from '../ui/input';
-import { useDebounceValue } from 'usehooks-ts';
-import { useSearchParams } from 'next/navigation';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import ComboBoxModelos from './ComboBoxModelos';
 
 export default function FiltroTabla() {
-  const [search, setSearch] = useDebounceValue('', 500);
   const searchParams = new URLSearchParams(useSearchParams());
-
+  const pathname = usePathname();
+  const router = useRouter();
   const { data: Grupos } = trpc.grupoEtiqueta.getAll.useQuery();
   return (
     <>
       <ComboBoxModelos data={Grupos ?? []} />
       <Input
         placeholder='Buscar por nombre o id legible'
-        onChange={(e) => {}}
+        onChange={(e) => {
+          searchParams.set('nombre', e.target.value);
+          router.push(`${pathname}?${searchParams.toString()}`);
+        }}
       />
     </>
   );
