6bebd2ae4c44c97fbcfd1da026bfdf77bffc512e
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2024-03-07 15:04:32 -0300

    add: Componentes de modelos a su página

--- src/app/(dashboard)/modelos/page.tsx
+++ src/app/(dashboard)/modelos/page.tsx
@@ -1,9 +1,13 @@
+import FiltroTabla from '@/components/modelos/FiltroTabla';
+import ModelosTabla from '@/components/modelos/ModelosTabla';
 import React from 'react';
 
 const ModelosPage = () => {
   return (
     <div>
       <p>ModelosPage</p>
+      <FiltroTabla />
+      <ModelosTabla />
     </div>
   );
 };
