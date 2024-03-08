13f3e1046d0801e6a338e27e82d3ffeade75e3ae
nicofishman <nicourifishman@gmail.com>
2024-03-08 16:47:33 -0300

    feat: Ordenamiento en las columnas importantes

--- src/components/modelos/table/columns.tsx
+++ src/components/modelos/table/columns.tsx
@@ -2,11 +2,14 @@
 
 import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
+import { getTextColorByBg } from '@/lib/utils';
 import { RouterOutputs } from '@/server';
-import { ColumnDef } from '@tanstack/react-table';
-import { ArrowDown, ArrowUp } from 'lucide-react';
+import { ColumnDef, SortDirection } from '@tanstack/react-table';
+import { ArrowDown, ArrowUp, ArrowUpDown } from 'lucide-react';
 
-export const columns: ColumnDef<RouterOutputs['modelo']['getAll'][number]>[] = [
+export const columns: ColumnDef<
+  RouterOutputs['modelo']['getByFiltro'][number]
+>[] = [
   {
     accessorKey: 'idLegible',
     header: ({ column }) => {
@@ -18,21 +21,20 @@ export const columns: ColumnDef<RouterOutputs['modelo']['getAll'][number]>[] = [
           }}
         >
           <Button
+            className='px-1'
             variant='ghost'
             onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
           >
             ID
-            {
-              {
-                asc: <ArrowDown className='ml-2 h-4 w-4 transform' />,
-                desc: <ArrowUp className='ml-2 h-4 w-4' />,
-              }[column.getIsSorted() === 'desc' ? 'desc' : 'asc']
-            }
+            <SortingIcon isSorted={column.getIsSorted()} />
           </Button>
         </div>
       );
     },
-    size: 70,
+    minSize: 50,
+    size: 50,
+    maxSize: 50,
+    enableResizing: false,
     cell: ({ row }) => {
       return <p className='w-full text-center'>{row.original.idLegible}</p>;
     },
@@ -47,12 +49,7 @@ export const columns: ColumnDef<RouterOutputs['modelo']['getAll'][number]>[] = [
           onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
         >
           Nombre
-          {
-            {
-              asc: <ArrowDown className='ml-2 h-4 w-4 transform' />,
-              desc: <ArrowUp className='ml-2 h-4 w-4' />,
-            }[column.getIsSorted() === 'desc' ? 'desc' : 'asc']
-          }
+          <SortingIcon isSorted={column.getIsSorted()} />
         </Button>
       );
     },
@@ -71,14 +68,45 @@ export const columns: ColumnDef<RouterOutputs['modelo']['getAll'][number]>[] = [
   },
   {
     accessorKey: 'etiquetas',
-    header: 'Etiquetas',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='ghost'
+          className='pl-0'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Etiquetas
+          <SortingIcon isSorted={column.getIsSorted()} />
+        </Button>
+      );
+    },
+    sortingFn: (a, b, isAsc) => {
+      // sort by number of etiquetas
+      const aCount = a.original.etiquetas.length;
+      const bCount = b.original.etiquetas.length;
+      if (aCount === bCount) {
+        return 0;
+      }
+      if (isAsc) {
+        return aCount > bCount ? 1 : -1;
+      } else {
+        return aCount < bCount ? 1 : -1;
+      }
+    },
     cell: ({ row }) => {
       const etiquetas = row.original.etiquetas;
 
       return (
-        <div>
+        <div className='flex flex-1'>
           {etiquetas.map((etiqueta) => (
-            <Badge key={etiqueta.id} color='primary' className='mr-1'>
+            <Badge
+              key={etiqueta.id}
+              style={{
+                backgroundColor: etiqueta.grupo.color,
+                color: getTextColorByBg(etiqueta.grupo.color),
+              }}
+              className='mr-1 whitespace-nowrap'
+            >
               {etiqueta.nombre}
             </Badge>
           ))}
@@ -87,3 +115,18 @@ export const columns: ColumnDef<RouterOutputs['modelo']['getAll'][number]>[] = [
     },
   },
 ];
+
+type SortingIconProps = {
+  isSorted: false | SortDirection;
+};
+
+const SortingIcon = ({ isSorted }: SortingIconProps) => {
+  return isSorted ? (
+    {
+      asc: <ArrowDown className='ml-2 h-4 w-4 transform' />,
+      desc: <ArrowUp className='ml-2 h-4 w-4' />,
+    }[isSorted === 'desc' ? 'desc' : 'asc']
+  ) : (
+    <ArrowUpDown className='ml-2 h-4 w-4' />
+  );
+};
