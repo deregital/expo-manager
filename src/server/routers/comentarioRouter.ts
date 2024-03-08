bf2c2e279f8ce2a7240f23603b8051f293f8da50
Alan Babor <baboralanoroynegro@gmail.com>
2024-03-08 14:30:34 -0300

    add: UPDATE y DELETE del router de comentario.

--- src/server/routers/comentarioRouter.ts
+++ src/server/routers/comentarioRouter.ts
@@ -58,7 +58,75 @@ export const comentarioRouter = router({
       return comentarios;
     }),
 
+    update: protectedProcedure
+    .input(
+      z.object({
+        id: z.string(), // Se requiere el ID del comentario a actualizar
+        contenido: z.string().min(1), 
+      })
+    )
+    .mutation(async ({ input, ctx }) => {
+      const userId = ctx.session?.user?.id;
+      if (!userId) {
+        throw new TRPCError({ code: 'UNAUTHORIZED', message: 'Usuario no autenticado' });
+      }
 
+      const comentario = await ctx.prisma.comentario.findUnique({
+        where: {
+          id: input.id,
+        },
+        select: {
+          creadoPor: true,
+        },
+      });
+
+      if (!comentario || comentario.creadoPor !== userId) {
+        throw new TRPCError({ code: 'FORBIDDEN', message: 'No tienes permiso para actualizar este comentario' });
+      }
+
+      return await ctx.prisma.comentario.update({
+        where: {
+          id: input.id,
+        },
+        data: {
+          contenido: input.contenido, 
+        },
+      });
+    }),
+
+
+    delete: protectedProcedure
+    .input(
+      z.object({
+        id: z.string(), // Se requiere el ID del comentario a borrar
+      })
+    )
+    .mutation(async ({ input, ctx }) => {
+      const userId = ctx.session?.user?.id;
+      if (!userId) {
+        throw new TRPCError({ code: 'UNAUTHORIZED', message: 'Usuario no autenticado' });
+      }
+
+      const comentario = await ctx.prisma.comentario.findUnique({
+        where: {
+          id: input.id,
+        },
+        select: {
+          creadoPor: true,
+        },
+      });
+
+      if (!comentario || comentario.creadoPor !== userId) {
+        throw new TRPCError({ code: 'FORBIDDEN', message: 'No tienes permiso para borrar este comentario' });
+      }
+
+      return await ctx.prisma.comentario.delete({
+        where: {
+          id: input.id,
+        },
+      }); 
+      
+    }),
 
   });
 
