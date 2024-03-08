146334a229925dc1cbf78c05931b6cc061489a0d
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2024-03-08 02:29:01 -0300

    add: Redirección desde grupo de etiquetas a la tabla

--- src/components/etiquetas/list/GrupoTrigger.tsx
+++ src/components/etiquetas/list/GrupoTrigger.tsx
@@ -1,5 +1,6 @@
 import GrupoEtiquetaModal from '@/components/etiquetas/modal/GrupoEtiquetaModal';
 import { RouterOutputs } from '@/server';
+import { useRouter, useSearchParams } from 'next/navigation';
 import React from 'react';
 
 interface GrupoTriggerProps {
@@ -10,6 +11,12 @@ interface GrupoTriggerProps {
 }
 
 const GrupoTrigger = ({ grupo }: GrupoTriggerProps) => {
+  const searchParams = new URLSearchParams(useSearchParams());
+  const router = useRouter();
+  function redirectTable() {
+    searchParams.set('grupoId', grupo.id);
+    router.push(`http://localhost:3000/modelos?${searchParams.toString()}`);
+  }
   return (
     <div className='flex w-full justify-between hover:no-underline'>
       <p className='pl-3 text-start font-bold'>{grupo.nombre}</p>
@@ -17,7 +24,7 @@ const GrupoTrigger = ({ grupo }: GrupoTriggerProps) => {
         <div onClick={(e) => e.preventDefault()}>
           <GrupoEtiquetaModal action='EDIT' grupo={grupo} />
         </div>
-        <p className='mr-2 text-sm font-semibold'>
+        <p className='mr-2 text-sm font-semibold' onClick={redirectTable}>
           {grupo._count.etiquetas}
           {' etiqueta'}
           {grupo._count.etiquetas > 1 ? 's' : ''}
