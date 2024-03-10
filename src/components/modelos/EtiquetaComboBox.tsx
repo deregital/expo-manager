9944ae370db9146cdf1da51205d8e2a415348fd7
nicofishman <nicourifishman@gmail.com>
2024-03-10 01:23:56 -0300

    feat: Aplicar nuevo ComboBox en modelosPage

--- src/components/modelos/EtiquetaComboBox.tsx
+++ src/components/modelos/EtiquetaComboBox.tsx
@@ -1,18 +1,9 @@
 'use client';
 import { useEffect, useState } from 'react';
-import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover';
-import { Button } from '../ui/button';
-import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-} from '../ui/command';
-import { CheckIcon } from 'lucide-react';
-import { cn } from '@/lib/utils';
+
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { trpc } from '@/lib/trpc';
+import ComboBox from '@/components/ui/ComboBox';
 
 const EtiquetaComboBoxModelos = () => {
   const [open, setOpen] = useState(false);
@@ -35,54 +26,31 @@ const EtiquetaComboBoxModelos = () => {
   }, [searchParams.get('etiqueta')]);
 
   return (
-    <Popover open={open} onOpenChange={setOpen}>
-      <PopoverTrigger className='text-black' asChild>
-        <Button
-          variant='outline'
-          role='combobox'
-          aria-expanded={open}
-          className='w-[200px] justify-between'
-        >
-          {etiquetaId
-            ? data?.find((etiqueta) => etiqueta.id === etiquetaId)?.nombre
-            : 'Buscar etiqueta...'}
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className='w-[200px] p-0'>
-        <Command>
-          <CommandInput placeholder='Buscar etiqueta...' className='h-9' />
-          <CommandEmpty>Etiqueta no encontrado.</CommandEmpty>
-          <CommandGroup>
-            {data?.map((etiqueta) => (
-              <CommandItem
-                className='cursor-pointer hover:bg-gray-100'
-                key={etiqueta.id}
-                value={etiqueta.nombre}
-                onSelect={() => {
-                  setOpen(false);
-                  if (etiqueta.id === etiquetaId) {
-                    setEtiquetaId('');
-                    searchParams.delete('etiqueta');
-                  } else {
-                    setEtiquetaId(etiqueta.id);
-                    searchParams.set('etiqueta', etiqueta.id);
-                  }
-                  router.push(`${pathname}?${searchParams.toString()}`);
-                }}
-              >
-                {etiqueta.nombre ? etiqueta.nombre : 'No hay nombre'}
-                <CheckIcon
-                  className={cn(
-                    'ml-auto h-4 w-4',
-                    etiquetaId === etiqueta.id ? 'opacity-100' : 'opacity-0'
-                  )}
-                />
-              </CommandItem>
-            ))}
-          </CommandGroup>
-        </Command>
-      </PopoverContent>
-    </Popover>
+    <ComboBox
+      open={open}
+      setOpen={setOpen}
+      triggerText={
+        etiquetaId
+          ? data?.find((etiqueta) => etiqueta.id === etiquetaId)?.nombre ??
+            'Buscar etiqueta...'
+          : 'Buscar etiqueta...'
+      }
+      data={data ?? []}
+      id='id'
+      value='nombre'
+      onSelect={(value) => {
+        setOpen(false);
+        if (value === etiquetaId) {
+          setEtiquetaId('');
+          searchParams.delete('etiqueta');
+        } else {
+          setEtiquetaId(value);
+          searchParams.set('etiqueta', value);
+        }
+        router.push(`${pathname}?${searchParams.toString()}`);
+      }}
+      selectedIf={etiquetaId}
+    />
   );
 };
 
