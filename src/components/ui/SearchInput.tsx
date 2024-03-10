1490f4064ce297f7bc475c7a587cd2c25e826006
nicofishman <nicourifishman@gmail.com>
2024-03-10 02:52:30 -0300

    move: SearchInput a carpeta UI

--- src/components/ui/SearchInput.tsx
+++ src/components/ui/SearchInput.tsx
@@ -2,20 +2,18 @@ import SearchIcon from '@/components/icons/SearchIcon';
 import { Input } from '@/components/ui/input';
 import React from 'react';
 
-interface SearchInputProps {
+interface SearchInputProps
+  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onChange'> {
   onChange: (_value: string) => void;
 }
 
-const SearchInput = ({ onChange }: SearchInputProps) => {
+const SearchInput = ({ onChange, ...props }: SearchInputProps) => {
   return (
     <div className='relative w-full md:max-w-md'>
       <span className='pointer-events-none absolute inset-y-0 right-0 flex cursor-crosshair items-center pr-3 text-muted-foreground'>
         <SearchIcon className='h-5 w-5' />
       </span>
-      <Input
-        placeholder='Buscar grupo o etiqueta'
-        onChange={(e) => onChange(e.target.value)}
-      />
+      <Input onChange={(e) => onChange(e.target.value)} {...props} />
     </div>
   );
 };
