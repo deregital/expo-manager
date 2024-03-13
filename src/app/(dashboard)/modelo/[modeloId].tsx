dab944a4135a9bc66f55728134d7d3b7138f5491
nicofishman <nicourifishman@gmail.com>
2024-03-13 10:18:58 -0300

    add: Ruta de modelo individual

---
+++ src/app/(dashboard)/modelo/[modeloId].tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface ModeloPageProps {}
+
+const ModeloPage = ({}: ModeloPageProps) => {
+  return (
+    <div>
+      <p>ModeloPage</p>
+    </div>
+  );
+};
+
+export default ModeloPage;
