d4f3eb4398702f968ebcdfe70dbfd6c426609c90
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2024-03-13 23:39:35 -0300

    fix: Retroceder a defaultValue en el input para elegir página

--- src/components/modelos/table/Pagination.tsx
+++ src/components/modelos/table/Pagination.tsx
@@ -57,7 +57,7 @@ const PaginationComp = <TData,>({ table }: PaginationProps<TData>) => {
             | Go to page:
             <input
               type='number'
-              value={table.getState().pagination.pageIndex + 1}
+              defaultValue={table.getState().pagination.pageIndex + 1}
               onChange={(e) => {
                 const page = e.target.value ? Number(e.target.value) - 1 : 0;
                 table.setPageIndex(page);
