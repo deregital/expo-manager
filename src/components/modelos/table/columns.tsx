07a3c29ccec91f7edc38ee7d9acae39e6f9ca0fd
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2024-03-14 15:51:06 -0300

    fix: Router Outputs

--- src/components/modelos/table/columns.tsx
+++ src/components/modelos/table/columns.tsx
@@ -8,7 +8,7 @@ import { ColumnDef, SortDirection } from '@tanstack/react-table';
 import { ArrowDown, ArrowUp, ArrowUpDown } from 'lucide-react';
 
 export const columns: ColumnDef<
-  RouterOutputs['modelo']['getByFiltro']['modelos'][number]
+  RouterOutputs['modelo']['getByFiltro'][number]
 >[] = [
   {
     accessorKey: 'idLegible',
