2e477d489f83ab3186f88b0834fa241bf70ed1bb
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2024-03-15 01:51:19 -0300

    feat:Cambio en edición de modelo para guardar su foto

--- src/server/routers/modelosRouter.ts
+++ src/server/routers/modelosRouter.ts
@@ -109,6 +109,7 @@ export const modeloRouter = router({
         nombreCompleto: z.string().optional(),
         nombrePila: z.string().optional(),
         telefono: z.string().optional(),
+        fotoUrl: z.string().optional(),
         genero: z.string().optional(),
         edad: z.number().optional(),
         etiquetas: z
@@ -149,6 +150,7 @@ export const modeloRouter = router({
           nombrePila: input.nombrePila,
           telefono: input.telefono,
           genero: input.genero,
+          fotoUrl: input.fotoUrl,
           edad: input.edad,
           etiquetas: {
             set: (input.etiquetas ?? []).map((etiqueta) => {
