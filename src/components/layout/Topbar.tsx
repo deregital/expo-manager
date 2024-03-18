eba088f2388ae297b75d23fba2d2ddd1a0defbf4
nicofishman <nicourifishman@gmail.com>
2024-03-18 12:34:57 -0300

    fix: Topbar session desde server

--- src/components/layout/Topbar.tsx
+++ src/components/layout/Topbar.tsx
@@ -1,10 +1,17 @@
 import SessionMenu from '@/components/layout/SessionMenu';
+import { getServerAuthSession } from '@/server/auth';
 import React from 'react';
 
-const Topbar = () => {
+const Topbar = async () => {
+  const session = await getServerAuthSession();
+
+  if (!session || !session.user) {
+    return null;
+  }
+
   return (
     <header className='flex h-16 items-center justify-end border-b-[3px] border-black/20 bg-topbar pr-4'>
-      <SessionMenu />
+      <SessionMenu user={session.user} />
     </header>
   );
 };
