362d3f347d6b92571138c1173ecbd69d00b40973
nicofishman <nicourifishman@gmail.com>
2024-03-18 22:10:28 -0300

    feat: Ahora anda

--- src/server/routers/csvRouter.ts
+++ src/server/routers/csvRouter.ts
@@ -1,68 +1,42 @@
-import { protectedProcedure, router } from "@/server/trpc";
-import { TRPCError } from "@trpc/server";
-import { PrismaClient } from '@prisma/client';
+import { protectedProcedure, router } from '@/server/trpc';
+import { TRPCError } from '@trpc/server';
 import * as fastCsv from 'fast-csv';
-
-const prisma = new PrismaClient();
-
-
-async function exportModelosToCSV() {
-  try {
-    const modelos = await prisma.perfil.findMany();
-
-    const csvStream = fastCsv.format({ headers: true });
-    let csvData = '';
-
-    csvStream.on('data', (chunk: any) => {
-      csvData += chunk;
-    });
-
-    modelos.forEach((row: any) => {
-      csvStream.write(row);
-    });
-
-    csvStream.end();
-
-    await new Promise<void>((resolve) => {
-      csvStream.on('end', () => {
-        resolve();
-      });
-    });
-
-    return csvData;
-  } catch (error) {
-    console.error('Error exporting to CSV:', error);
-    throw error;
-  } finally {
-    await prisma.$disconnect(); 
-  }
-}
-
-
-export const exportModelos = protectedProcedure.mutation(async ({ ctx }) => {
-  if (!ctx.session?.user) {
-    throw new TRPCError({
-      code: "UNAUTHORIZED",
-      message: "No tienes permisos para realizar esta acción",
-    });
-  }
-
- 
-  return exportModelosToCSV();
-});
-
 
 export const csvRouter = router({
   downloadModelos: protectedProcedure.mutation(async ({ ctx }) => {
     if (!ctx.session?.user) {
       throw new TRPCError({
-        code: "UNAUTHORIZED",
-        message: "No tienes permisos para realizar esta acción",
+        code: 'UNAUTHORIZED',
+        message: 'No tienes permisos para realizar esta acción',
       });
     }
 
-   
-    return exportModelos;
+    try {
+      const modelos = await ctx.prisma.perfil.findMany();
+
+      const csvStream = fastCsv.format({ headers: true });
+      let csvData = '';
+
+      csvStream.on('data', (chunk: any) => {
+        csvData += chunk;
+      });
+
+      modelos.forEach((row: any) => {
+        csvStream.write(row);
+      });
+
+      csvStream.end();
+
+      await new Promise<void>((resolve) => {
+        csvStream.on('end', () => {
+          resolve();
+        });
+      });
+
+      return csvData;
+    } catch (error) {
+      console.error('Error exporting to CSV:', error);
+      throw error;
+    }
   }),
 });
-
