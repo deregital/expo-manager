2dfa3f167543dc79514f662076d92a62ad2764c0
nicofishman <nicourifishman@gmail.com>
2024-03-21 18:41:22 -0300

    23- Link de next para redirigir a la tabla filtrada

--- src/components/etiquetas/list/EtiquetasContent.tsx
+++ src/components/etiquetas/list/EtiquetasContent.tsx
@@ -2,7 +2,7 @@ import { GrupoConMatch } from '@/components/etiquetas/list/EtiquetasList';
 import EtiquetaModal from '@/components/etiquetas/modal/EtiquetaModal';
 import ModeloIcon from '@/components/icons/ModeloIcon';
 import { cn } from '@/lib/utils';
-import { useRouter, useSearchParams } from 'next/navigation';
+import Link from 'next/link';
 import React from 'react';
 
 interface EtiquetasContentProps {
@@ -16,16 +16,6 @@ const EtiquetasContent = ({
   background,
   grupoId,
 }: EtiquetasContentProps) => {
-  const searchParams = new URLSearchParams(useSearchParams());
-  const router = useRouter();
-
-  function redirectTable(e: React.MouseEvent<SVGElement>) {
-    e.preventDefault();
-    searchParams.set('etiqueta', etiqueta.id);
-    searchParams.set('grupoId', grupoId);
-    router.push(`/modelos?${searchParams.toString()}`);
-  }
-
   return (
     <div
       className='mb-2 ml-1.5 mt-1.5 flex justify-between rounded-md px-4 py-2 text-black shadow-md shadow-black/30'
@@ -40,10 +30,9 @@ const EtiquetasContent = ({
       <div className='flex items-center gap-x-2'>
         <EtiquetaModal action='EDIT' etiqueta={etiqueta} />
         <p className='text-sm font-semibold'>{etiqueta._count.perfiles}</p>
-        <ModeloIcon
-          className='h-4 w-4 hover:cursor-pointer hover:text-gray-700'
-          onClick={(event) => redirectTable(event)}
-        />
+        <Link href={`/modelos?etiqueta=${etiqueta.id}&grupoId=${grupoId}`}>
+          <ModeloIcon className='h-4 w-4 hover:cursor-pointer hover:text-gray-700' />
+        </Link>
       </div>
     </div>
   );
