e96a9dc06360a7ccbe11e52fc8f8f6ec0b1204c5
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2024-03-26 19:42:04 -0300

    add: Agarrar la data de todas las tablas y mostrarla

--- src/app/(dashboard)/configuracion/page.tsx
+++ src/app/(dashboard)/configuracion/page.tsx
@@ -6,6 +6,8 @@ import React from 'react';
 const ConfiguracionPage = () => {
   const exportModelos = trpc.csv.downloadModelos.useMutation();
 
+  const { data } = trpc.csv.downloadDatabase.useQuery();
+  console.log(data);
   const handleDownloadCSV = async () => {
     try {
       const csvData = await exportModelos.mutateAsync(); 
@@ -29,24 +31,25 @@ const ConfiguracionPage = () => {
 
   return (
     <div>
-      <p>ConfiguracionPage</p>
+      <p className='px-5'>ConfiguracionPage</p>
       {}
-      <div style={{ marginTop: '20px' }}>
+      <div className='mt-5 px-5'>
         {/* Botón para descargar el archivo CSV con estilos */}
         <button
-          onClick={handleDownloadCSV} 
-          style={{
-            padding: '10px 20px',
-            backgroundColor: '#007bff',
-            color: '#fff',
-            border: 'none',
-            borderRadius: '5px',
-            cursor: 'pointer',
-            fontSize: '16px',
-            fontWeight: 'bold',
-            boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
-            transition: 'background-color 0.3s',
-          }}
+          onClick={handleDownloadCSV}
+          className='px-4 py-2 bg-blue-500 text-white rounded cursor-pointer text-lg font-bold shadow-md transition duration-300 hover:bg-blue-600'
+          // style={{
+          //   padding: '10px 20px',
+          //   backgroundColor: '#007bff',
+          //   color: '#fff',
+          //   border: 'none',
+          //   borderRadius: '5px',
+          //   cursor: 'pointer',
+          //   fontSize: '16px',
+          //   fontWeight: 'bold',
+          //   boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+          //   transition: 'background-color 0.3s',
+          // }}
         >
           Descargar Modelos
         </button>
