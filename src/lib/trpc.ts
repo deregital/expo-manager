779c2c4c43050abb18b3723fc18285a19e346adb
nicofishman <nicourifishman@gmail.com>
2024-04-04 00:53:36 -0300

    fix: url para hacer requests a trpc funciona en build

--- src/lib/trpc.ts
+++ src/lib/trpc.ts
@@ -33,7 +33,7 @@ export const trpc = createTRPCNext<AppRouter>({
            * If you want to use SSR, you need to use the server's full URL
            * @link https://trpc.io/docs/v11/ssr
            **/
-          url: `${getBaseUrl()}/api/trpc`,
+          url: `/api/trpc`,
           // You can pass any HTTP headers you wish here
           async headers() {
             return {
