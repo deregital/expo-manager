47d106703e00f67827568cb499cf50c0c1682bbf
nicofishman <nicourifishman@gmail.com>
2024-04-04 23:55:17 -0300

    feat: Removido el header Access-Control-Allow-Credentials

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [{protocol: 'https', hostname:'expo-manager.b-cdn.net'}],
-    },
-    async headers() {
-        return [
+  images: {
+    remotePatterns: [{ protocol: 'https', hostname: 'expo-manager.b-cdn.net' }],
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: '/api/formulario',
+        headers: [
+          //   { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
           {
-            // matching all API routes
-            source: "/api/formulario",
-            headers: [
-              { key: "Access-Control-Allow-Credentials", value: "true" },
-              { key: "Access-Control-Allow-Origin", value: "*" },
-              { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-              { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-            ]
-          }
-        ]
-      }
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
