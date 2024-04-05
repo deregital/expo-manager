67062b5fe15ae5ab25e4ae7b33fb177aefc53154
nicofishman <nicourifishman@gmail.com>
2024-04-05 00:48:13 -0300

    feat: Importar prisma para no generar tantas conexiones

--- src/app/api/formulario/route.ts
+++ src/app/api/formulario/route.ts
@@ -1,33 +1,41 @@
-import { NextRequest, NextResponse } from "next/server";
-
-
+import { prisma } from '@/server/db';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(req: NextRequest, res: NextResponse) {
-    try {
-        const data = await req.json();
-        const telefonoSinSeparaciones = data.telefono.replace(/\s+/g, '').replace(/\+/g, '');
-        const telefonoExistente = await prisma?.perfil.findFirst({
-            where: {
-                telefono: telefonoSinSeparaciones
-            }
-        });
-        if (telefonoExistente) {
-            return NextResponse.json({error: 'El teléfono ya está registrado'}, {status: 400});
-        }
-        const nombrePila = data.nombreCompleto.split(' ')[0];
-        const response = await prisma?.perfil.create({
-            data: {
-                nombreCompleto: data.nombreCompleto,
-                nombrePila: nombrePila,
-                telefono: telefonoSinSeparaciones,
-                etiquetas: {
-                    connect: {id: process.env.MODELO_ETIQUETA_ID}
-                }
-            }
-        });
-        return NextResponse.json(response, {status: 201});
-    } catch (error) {
-        console.error('Error creating new profile:', error);
-        return NextResponse.json({error: 'Error creating new profile'}, {status: 500});
+  try {
+    const data = await req.json();
+    const telefonoSinSeparaciones = data.telefono
+      .replace(/\s+/g, '')
+      .replace(/\+/g, '');
+    const telefonoExistente = await prisma?.perfil.findFirst({
+      where: {
+        telefono: telefonoSinSeparaciones,
+      },
+    });
+    if (telefonoExistente) {
+      return NextResponse.json(
+        { error: 'El teléfono ya está registrado' },
+        { status: 400 }
+      );
     }
+    const nombrePila = data.nombreCompleto.split(' ')[0];
+    const response = await prisma?.perfil.create({
+      data: {
+        nombreCompleto: data.nombreCompleto,
+        nombrePila: nombrePila,
+        telefono: telefonoSinSeparaciones,
+        etiquetas: {
+          connect: { id: process.env.MODELO_ETIQUETA_ID },
+        },
+      },
+    });
+
+    return NextResponse.json(response, { status: 201 });
+  } catch (error) {
+    console.error('Error creating new profile:', error);
+    return NextResponse.json(
+      { error: 'Error creating new profile' },
+      { status: 500 }
+    );
+  }
 }
