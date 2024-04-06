181ce08e1c97fd1718648deef5394af3ef2e8922
nicofishman <nicourifishman@gmail.com>
2024-04-06 10:55:15 -0300

    feat: Tooltip custom

--- src/components/dashboard/BarChart.tsx
+++ src/components/dashboard/BarChart.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React from 'react';
 import {
   Bar,
@@ -7,6 +8,11 @@ import {
   YAxis,
   BarChart as RechartChart,
 } from 'recharts';
+import {
+  NameType,
+  Payload,
+  ValueType,
+} from 'recharts/types/component/DefaultTooltipContent';
 import { create } from 'zustand';
 
 interface BarChartProps {
@@ -23,18 +29,57 @@ export const modelosData = create<{
 }));
 
 const BarChart = ({ data }: BarChartProps) => {
+  const error = console.error;
+  console.error = (...args: any) => {
+    if (/defaultProps/.test(args[0])) return;
+    error(...args);
+  };
   return (
-    <RechartChart data={data} width={500} height={300}>
+    <RechartChart id='modelosChart' data={data} width={500} height={300}>
       <XAxis dataKey='fecha' />
       <YAxis />
-      <Tooltip />
+      <Tooltip
+        content={({ active, payload, label }) => (
+          <CustomTooltip
+            text={'Modelos creadas'}
+            active={active}
+            payload={payload}
+            label={label}
+          />
+        )}
+      />
       <Bar
         dataKey='modelos'
         className='fill-slate-700'
-        activeBar={<Rectangle fill='pink' stroke='blue' />}
+        activeBar={<Rectangle stroke='black' />}
       />
     </RechartChart>
   );
 };
 
 export default BarChart;
+
+const CustomTooltip = ({
+  active,
+  payload,
+  label,
+  text,
+}: {
+  active?: boolean;
+  payload?: Payload<ValueType, NameType>[] | undefined;
+  label?: string;
+  text: string;
+}) => {
+  return (
+    active &&
+    payload &&
+    payload.length && (
+      <div className='rounded-md bg-white/90 p-2'>
+        <p>{label}</p>
+        <p>
+          {text}: {payload[0].value}
+        </p>
+      </div>
+    )
+  );
+};
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -68,7 +68,7 @@ export function searchNormalize(str: string, search: string) {
 }
 
 export function dateFormatYYYYMMDD(date: Date) {
-  return `${date.getFullYear()}-${date.getMonth() + 1 > 10 ? date.getMonth() + 1 : `0${date.getMonth() + 1}`}-${date.getDate()}`;
+  return `${date.getFullYear()}-${date.getMonth() + 1 > 10 ? date.getMonth() + 1 : `0${date.getMonth() + 1}`}-${date.getDate() > 10 ? date.getDate() : `0${date.getDate()}`}`;
 }
 
 export function addDays(date: string, days: number) {
