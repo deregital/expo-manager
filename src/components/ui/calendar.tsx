9ae39c4265770441d13934fa8ed578f48c2953a1
nicofishman <nicourifishman@gmail.com>
2024-04-07 13:03:59 -0300

    feat: Puntito rojo al día de hoy en el calendario

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -44,7 +44,8 @@ const Calendar = ({
         day_range_end: 'day-range-end',
         day_selected:
           'bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground',
-        day_today: 'bg-accent text-accent-foreground',
+        day_today:
+          'bg-accent text-accent-foreground after:w-1 after:h-1 after:rounded-full after:absolute after:bottom-1 after:mx-auto after:bg-red-500',
         day_outside:
           'day-outside text-muted-foreground opacity-50 aria-selected:bg-accent/50 aria-selected:text-muted-foreground aria-selected:opacity-30',
         day_disabled: 'text-muted-foreground opacity-50',
