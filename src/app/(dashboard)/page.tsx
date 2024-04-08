e67414e06036568b1b4606704dbd098943675e95
nicofishman <nicourifishman@gmail.com>
2024-04-08 12:47:25 -0300

    feat: Layout para desktop y mobile

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,10 +1,58 @@
 'use client';
-import Greeting from '@/components/Greeting';
+
+import { DateRangePicker } from '@/components/ui/DateRangePicker';
+import { addDays, dateFormatYYYYMMDD } from '@/lib/utils';
+import { create } from 'zustand';
+
+export const useDashboardData = create<{
+  from: string;
+  to: string;
+  etiquetaId: string | null;
+}>(() => ({
+  from: dateFormatYYYYMMDD(new Date()),
+  to: dateFormatYYYYMMDD(addDays(new Date().toISOString(), 1)),
+  etiquetaId: null,
+}));
 
 const Home = () => {
+  const { from, to } = useDashboardData();
+
   return (
-    <main className='flex h-full flex-col items-center justify-between p-24'>
-      <Greeting />
+    <main className='grid h-full grid-cols-1 grid-rows-[10%,auto,25%] gap-2 p-5 grid-areas-dashboardSmall sm:grid-cols-3 sm:grid-areas-dashboardLarge'>
+      <section className='grid-in-calendar'>
+        <DateRangePicker
+          align='start'
+          showCompare={false}
+          initialDateFrom={from}
+          initialDateTo={to}
+          locale='es-AR'
+        />
+      </section>
+      <section className='w-full grid-in-grupo'>
+        <div className='h-full w-full rounded-md bg-white shadow-md'>
+          Grupo dropdown
+        </div>
+      </section>
+      <section className='w-full grid-in-etiqueta'>
+        <div className='h-full w-full rounded-md bg-white shadow-md'>
+          Etiqueta dropdown
+        </div>
+      </section>
+      <section className='rounded-md bg-red-500 shadow-md grid-in-grafico'>
+        Grafico
+      </section>
+      <section className='rounded-md bg-green-500 shadow-md grid-in-listaModelos'>
+        Lista de modelos
+      </section>
+      <section className='rounded-md bg-yellow-500 shadow-md grid-in-cardModelos'>
+        Card Modelos participando
+      </section>
+      <section className='rounded-md bg-blue-500 shadow-md grid-in-cardRetencion'>
+        Card Retención de modelos
+      </section>
+      <section className='rounded-md bg-cyan-500 shadow-md grid-in-cardMensajes'>
+        Card Mensajes
+      </section>
     </main>
   );
 };
