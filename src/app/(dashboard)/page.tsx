b42e3a904df9a5b983a9adc2fd3c9a549f4c2d0e
nicofishman <nicourifishman@gmail.com>
2024-04-11 19:10:59 -0300

    fix

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -5,7 +5,7 @@ import PageClient from '@/components/dashboard/PageClient';
 const Home = () => {
   return (
     <main className='grid h-full grid-cols-1 gap-2 p-5 grid-areas-dashboardSmall sm:grid-cols-3 sm:grid-rows-[10%,65%,25%] sm:grid-areas-dashboardLarge [&_section]:h-min sm:[&_section]:h-auto'>
-     <PageClient/> 
+      <PageClient />
     </main>
   );
 };
