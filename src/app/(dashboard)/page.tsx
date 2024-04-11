0bc8991e8e1bf80b362ede35f22caae45df3e272
Alan Babor <baboralanoroynegro@gmail.com>
2024-04-11 18:18:05 -0300

    add: componente greeting

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,11 +1,12 @@
 'use client';
 
+import Greeting from '@/components/dashboard/Greeting';
 import PageClient from '@/components/dashboard/PageClient';
 
 const Home = () => {
   return (
     <main className='grid h-full grid-cols-1 gap-2 p-5 grid-areas-dashboardSmall sm:grid-cols-3 sm:grid-rows-[10%,65%,25%] sm:grid-areas-dashboardLarge [&_section]:h-min sm:[&_section]:h-auto'>
-      <PageClient />
+     <Greeting/> 
     </main>
   );
 };
