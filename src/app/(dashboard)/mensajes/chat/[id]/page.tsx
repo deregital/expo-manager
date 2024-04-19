9c3752fcf6fbec19107e28f93f12946acfcd3af0
nicofishman <nicourifishman@gmail.com>
2024-04-19 14:22:31 -0300

    feat: rutas de chat

---
+++ src/app/(dashboard)/mensajes/chat/[id]/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface ChatPageProps {}
+
+const ChatPage = ({}: ChatPageProps) => {
+  return (
+    <div>
+      <p>ChatPage</p>
+    </div>
+  );
+};
+
+export default ChatPage;
---
+++ src/app/(dashboard)/mensajes/chat/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface NoSelectedChatPageProps {}
+
+const NoSelectedChatPage = ({}: NoSelectedChatPageProps) => {
+  return (
+    <div>
+      <p> NoSelectedChatPage</p>
+    </div>
+  );
+};
+
+export default NoSelectedChatPage;
