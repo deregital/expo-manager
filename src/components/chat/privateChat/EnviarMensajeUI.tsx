8c8f94008831e9d278b05fbd30f5da0f1ef4420e
nicofishman <nicourifishman@gmail.com>
2024-04-21 11:18:55 -0300

    feat: Input para mandar mensaje

--- src/components/chat/privateChat/EnviarMensajeUI.tsx
+++ src/components/chat/privateChat/EnviarMensajeUI.tsx
@@ -4,16 +4,24 @@ import React, { useState } from 'react';
 
 interface EnviarMensajeUIProps {
   telefono: string;
+  inChat: boolean;
 }
 
-const EnviarMensajeUI = ({ telefono }: EnviarMensajeUIProps) => {
-  const sendMessage = trpc.whatsapp.sendMessageToTelefono.useMutation();
+const EnviarMensajeUI = ({ telefono, inChat }: EnviarMensajeUIProps) => {
+  const utils = trpc.useUtils();
+  const sendMessage = trpc.whatsapp.sendMessageToTelefono.useMutation({
+    onSuccess: () => {
+      setMessage('');
+      utils.whatsapp.getMessagesByTelefono.invalidate(telefono);
+    },
+  });
   const [message, setMessage] = useState('');
 
   return (
     <form
-      className='flex flex-row gap-4 bg-[#f0f2f5] px-4 py-2'
+      className='z-10 flex flex-row gap-4 bg-[#f0f2f5] px-4 py-2'
       onSubmit={(event) => {
+        if (!inChat) return;
         event.preventDefault();
         sendMessage.mutateAsync({
           telefono,
@@ -22,12 +30,19 @@ const EnviarMensajeUI = ({ telefono }: EnviarMensajeUIProps) => {
       }}
     >
       <input
+        disabled={!inChat}
         value={message}
         onChange={(e) => setMessage(e.target.value)}
         className='w-full rounded-md p-2'
-        placeholder='Type a message'
+        placeholder={
+          inChat
+            ? 'Escribí un mensaje'
+            : 'No se pueden mandar mensajes ahora, mandale una plantilla antes'
+        }
       />
-      <Button type='submit'>Send</Button>
+      <Button disabled={!inChat} type='submit'>
+        Enviar
+      </Button>
     </form>
   );
 };
