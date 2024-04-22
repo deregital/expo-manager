4bfa3c8ade8f32d79e08fcf2660cb796c65bc03d
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2024-04-22 12:15:17 -0300

    add: Funcionalidad para armar la lista de etiquetas

--- src/components/mensajes/EnviarTemplate.tsx
+++ src/components/mensajes/EnviarTemplate.tsx
@@ -4,10 +4,17 @@ import { trpc } from '@/lib/trpc';
 import ComboBox from '../ui/ComboBox';
 import { useMemo, useState } from 'react';
 import TemplateIcon from '../icons/TemplateIcon';
+import EtiquetaFillIcon from '../icons/EtiquetaFillIcon';
+import { toast } from 'sonner';
+
+interface EtiquetasProps {
+  id: string;
+  name: string;
+}
 
 export const useEnviarTemplate = create<{
   plantilla: string;
-  etiquetas: string[];
+  etiquetas: EtiquetasProps[];
 }>(() => ({
   plantilla: '',
   etiquetas: [],
@@ -19,7 +26,9 @@ const EnviarTemplate = () => {
     etiquetas: state.etiquetas,
   }));
   const { data } = trpc.whatsapp.getTemplates.useQuery();
-  const [open, setOpen] = useState(false);
+  const { data: etiquetas } = trpc.etiqueta.getAll.useQuery();
+  const [openPlantilla, setOpenPlantilla] = useState(false);
+  const [openEtiqueta, setOpenEtiqueta] = useState(false);
 
   const currentPlantilla = useMemo(() => {
     return data?.data.find((plantilla) => plantilla.id === templateData.plantilla)?.name;
@@ -31,24 +40,75 @@ const EnviarTemplate = () => {
         data={data?.data ? data.data : []}
         id={'id'}
         value={'name'}
-        open={open}
-        setOpen={setOpen}
+        open={openPlantilla}
+        setOpen={setOpenPlantilla}
         triggerChildren={
           <>
             <span className='max-w-[calc(100%-30px)] truncate'>
-              {templateData.plantilla ? currentPlantilla : 'Buscar plantilla...'}
+              {templateData.plantilla !== '' ? currentPlantilla : 'Buscar plantilla...'}
             </span>
             <TemplateIcon className='h-5 w-5' />
           </>
         }
         onSelect={(id) => {
+          if (templateData.plantilla === id) {
+            useEnviarTemplate.setState({
+              plantilla: '',
+            });
+            setOpenPlantilla(false);
+            return;
+          }
           useEnviarTemplate.setState({
             plantilla: id,
           });
-          setOpen(false);
+          setOpenPlantilla(false);
         }}
         selectedIf={templateData.plantilla}
         wFullMobile 
+        />
+        {templateData.etiquetas.map((etiqueta) => (
+          <div key={etiqueta.id} className='flex justify-between items-center'>
+            <span>{etiqueta.name}</span>
+            <button
+              onClick={() => {
+                useEnviarTemplate.setState({
+                  etiquetas: templateData.etiquetas.filter((et) => et.id !== etiqueta.id),
+                });
+              }}
+              className='p-1 text-red-500'
+            >
+              Eliminar
+            </button>
+            </div>
+        ))}
+        <ComboBox
+          data={etiquetas ? etiquetas : []}
+          id={'id'}
+          value={'nombre'}
+          open={openEtiqueta}
+          setOpen={setOpenEtiqueta}
+          triggerChildren={
+            <>
+              <span className='max-w-[calc(100%-30px)] truncate'>
+                {templateData.etiquetas.length > 0 ? 'Buscar etiqueta...' : 'Buscar etiqueta...'}
+              </span>
+              <EtiquetaFillIcon className='h-5 w-5' />
+            </>
+          }
+          onSelect={(id) => {
+            if (templateData.etiquetas.find((et) => et.id === id)) {
+              toast.error('La etiqueta ya ha sido seleccionada');
+              setOpenEtiqueta(false);
+              return
+            }
+            useEnviarTemplate.setState({
+              etiquetas: [...templateData.etiquetas, { id, name: etiquetas?.find((et) => et.id === id)?.nombre ?? ''}],
+            });
+            setOpenEtiqueta(false);
+            toast.success('Etiqueta agregada correctamente');
+          }}
+          selectedIf={''}
+          wFullMobile
         />
     </div>
   );
