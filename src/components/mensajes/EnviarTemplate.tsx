ac3cf8a0f8a6f2ba146637d063de96c453dd59d1
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2024-04-22 00:08:18 -0300

    add: ComboBox para seleccionar plantilla

--- src/components/mensajes/EnviarTemplate.tsx
+++ src/components/mensajes/EnviarTemplate.tsx
@@ -1,6 +1,9 @@
 'use client';
 import { create } from 'zustand';
 import { trpc } from '@/lib/trpc';
+import ComboBox from '../ui/ComboBox';
+import { useMemo, useState } from 'react';
+import TemplateIcon from '../icons/TemplateIcon';
 
 export const useEnviarTemplate = create<{
   plantilla: string;
@@ -11,11 +14,42 @@ export const useEnviarTemplate = create<{
 }));
 
 const EnviarTemplate = () => {
+  const templateData = useEnviarTemplate((state) => ({
+    plantilla: state.plantilla,
+    etiquetas: state.etiquetas,
+  }));
   const { data } = trpc.whatsapp.getTemplates.useQuery();
+  const [open, setOpen] = useState(false);
+
+  const currentPlantilla = useMemo(() => {
+    return data?.data.find((plantilla) => plantilla.id === templateData.plantilla)?.name;
+  }, [data, templateData.plantilla]);
+
   return (
-    <div>
-      <h1>Enviar Template</h1>
-      {/* <ComboBoxPlantillas data={data as Array<any>} /> */}
+    <div className='p-5'>
+      <ComboBox 
+        data={data?.data ? data.data : []}
+        id={'id'}
+        value={'name'}
+        open={open}
+        setOpen={setOpen}
+        triggerChildren={
+          <>
+            <span className='max-w-[calc(100%-30px)] truncate'>
+              {templateData.plantilla ? currentPlantilla : 'Buscar plantilla...'}
+            </span>
+            <TemplateIcon className='h-5 w-5' />
+          </>
+        }
+        onSelect={(id) => {
+          useEnviarTemplate.setState({
+            plantilla: id,
+          });
+          setOpen(false);
+        }}
+        selectedIf={templateData.plantilla}
+        wFullMobile 
+        />
     </div>
   );
 };
