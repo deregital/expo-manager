aaf32f4c131a66fbc325363efad1ff65040cba16
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2024-05-02 18:36:32 -0300

    feat: Empiezo de presentismo

--- src/app/(dashboard)/eventos/[eventoId]/presentismo/page.tsx
+++ src/app/(dashboard)/eventos/[eventoId]/presentismo/page.tsx
@@ -1,5 +1,58 @@
-const PresentismoPage = () => {
-  return <div>Presentismo</div>;
+'use client'
+import { generateColumnsPresentismo } from "@/components/eventos/table/columnsPresentismo";
+import { DataTable } from "@/components/modelos/table/dataTable";
+import Loader from "@/components/ui/loader";
+import { trpc } from "@/lib/trpc";
+import { format } from "date-fns";
+import { ArrowLeftIcon } from "lucide-react";
+
+interface PresentismoPageProps {
+  params: {
+    eventoId: string;
+  }
+}
+
+const PresentismoPage = ({params}: PresentismoPageProps) => {
+  const {data: evento, isLoading: isLoadingEvento} = trpc.evento.getById.useQuery({
+    id: params.eventoId
+  });
+  const {data: modelos} = trpc.modelo.getByEtiqueta.useQuery([evento!.etiquetaConfirmoId], {
+    enabled: !!evento
+  });
+  if (isLoadingEvento)
+    return (
+      <div className='flex items-center justify-center pt-5'>
+        <Loader />
+      </div>
+    );
+
+  return (
+    <div>
+      <div>
+        <ArrowLeftIcon
+          className='h-10 w-10 pt-3 hover:cursor-pointer'
+          onClick={() => {
+            window.history.back();
+          }}
+        />
+      </div>
+      <div className="flex justify-center items-center pb-3">
+        <h1 className="text-3xl font-extrabold">Presentismo</h1>
+      </div>
+      <div className='grid auto-rows-auto grid-cols-2 items-center justify-center gap-x-3 pb-3 sm:flex'>
+        <h3 className='col-span-2 p-2 text-center text-2xl font-semibold'>
+          {evento?.nombre}
+        </h3>
+        <h3 className='p-2 text-center text-sm sm:text-base'>
+          {format(evento!.fecha, 'yyyy-MM-dd')}
+        </h3>
+        <h3 className='p-2 text-center text-sm sm:text-base'>
+          {evento?.ubicacion}
+        </h3>
+      </div>
+      {/* <DataTable columns={generateColumnsPresentismo(params.eventoId)} data={modelos} /> */}
+    </div>
+  );
 };
 
 export default PresentismoPage;
---
+++ src/components/eventos/table/columnsPresentismo.tsx
@@ -0,0 +1,106 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { RouterOutputs } from '@/server';
+import { ColumnDef, SortDirection } from '@tanstack/react-table';
+import { ArrowDown, ArrowUp, ArrowUpDown } from 'lucide-react';
+import CellComponent from './CellComponent';
+
+export function generateColumnsPresentismo(id: string) {
+  const columns: ColumnDef<RouterOutputs['modelo']['getAll'][number]>[] = [
+    {
+      accessorKey: 'idLegible',
+      header: ({ column }) => {
+        return (
+          <div
+            className='mx-auto w-full'
+            style={{
+              width: `${column.getSize()}px`,
+            }}
+          >
+            <Button
+              className='px-1'
+              variant='ghost'
+              onClick={() =>
+                column.toggleSorting(column.getIsSorted() === 'asc')
+              }
+            >
+              ID
+              <SortingIcon isSorted={column.getIsSorted()} />
+            </Button>
+          </div>
+        );
+      },
+      minSize: 50,
+      size: 50,
+      maxSize: 50,
+      enableResizing: false,
+      cell: ({ row }) => {
+        return <p className='w-full text-center'>{row.original.idLegible}</p>;
+      },
+    },
+    {
+      accessorKey: 'nombreCompleto',
+      header: ({ column }) => {
+        return (
+          <Button
+            variant='ghost'
+            className='pl-0'
+            onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+          >
+            Nombre
+            <SortingIcon isSorted={column.getIsSorted()} />
+          </Button>
+        );
+      },
+    },
+    {
+      accessorKey: '¿Vino?',
+      cell: ({ row }) => <CellComponent row={row} confirmoAsistenciaId={id} />,
+      sortingFn: (rowA, rowB) => {
+        // This is a custom sorting function that sorts rows that contain id in etiquetas first
+        const a = rowA.original.etiquetas.map((etiqueta) => etiqueta.id);
+        const b = rowB.original.etiquetas.map((etiqueta) => etiqueta.id);
+
+        const hasEtiquetaA = a.includes(id);
+        const hasEtiquetaB = b.includes(id);
+
+        if (hasEtiquetaA && !hasEtiquetaB) return -1;
+        if (!hasEtiquetaA && hasEtiquetaB) return 1;
+        return 0;
+      },
+      header: ({ column }) => {
+        return (
+          <div className='flex justify-center'>
+            <Button
+              variant='ghost'
+              className='pl-0'
+              onClick={() =>
+                column.toggleSorting(column.getIsSorted() === 'asc')
+              }
+            >
+              ¿Vino?
+              <SortingIcon isSorted={column.getIsSorted()} />
+            </Button>
+          </div>
+        );
+      },
+    },
+  ];
+  return columns;
+}
+
+type SortingIconProps = {
+  isSorted: false | SortDirection;
+};
+
+const SortingIcon = ({ isSorted }: SortingIconProps) => {
+  return isSorted ? (
+    {
+      asc: <ArrowDown className='ml-2 h-4 w-4 transform' />,
+      desc: <ArrowUp className='ml-2 h-4 w-4' />,
+    }[isSorted === 'desc' ? 'desc' : 'asc']
+  ) : (
+    <ArrowUpDown className='ml-2 h-4 w-4' />
+  );
+};
