fe02ce33aa96716123f0a2dde0706ba7fd916d3b
Alan Babor <baboralanoroynegro@gmail.com>
2024-06-12 21:44:19 -0300

    feat: ocultar-mostrar contraseña

--- src/components/config/ModalPassword.tsx
+++ src/components/config/ModalPassword.tsx
@@ -13,6 +13,7 @@ const ModalPassword: React.FC<ModalPasswordProps> = ({
   onSubmit,
 }) => {
   const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
@@ -28,13 +29,22 @@ const ModalPassword: React.FC<ModalPasswordProps> = ({
       <div className='w-80 rounded bg-white p-6 shadow-md'>
         <h2 className='mb-4 text-2xl'>Ingresa tu contraseña</h2>
         <form onSubmit={handleSubmit}>
-          <input
-            type='password'
-            className='mb-4 w-full rounded border p-2'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
+          <div className='relative mb-4'>
+            <input
+              type={showPassword ? 'text' : 'password'}
+              className='w-full rounded border p-2'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <button
+              type='button'
+              className='absolute inset-y-0 right-0 px-4 py-2 text-sm text-gray-600'
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {showPassword ? 'Ocultar' : 'Mostrar'}
+            </button>
+          </div>
           <div className='flex justify-end'>
             <button
               type='button'
