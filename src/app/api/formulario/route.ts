f62cb3e872a51de2189c34856ac354228f7bb549
Alan Babor <baboralanoroynegro@gmail.com>
2024-08-14 20:09:53 -0300

    add: FECHA NACIMIENTO AL MANAGER

--- src/app/api/formulario/route.ts
+++ src/app/api/formulario/route.ts
@@ -22,6 +22,10 @@ const schema = z.object({
   genero: z.string().min(1, 'El género es requerido').optional(),
   mail: z.string().email('El correo electrónico no es válido').optional(),
   instagram: z.string().min(1, 'El Instagram es requerido').optional(),
+  fechaNacimiento: z
+    .string()
+    .min(1, 'La fecha de nacimiento es requerida')
+    .optional(),
 });
 
 export async function POST(req: NextRequest, res: NextResponse) {
@@ -37,6 +41,7 @@ export async function POST(req: NextRequest, res: NextResponse) {
       genero,
       mail,
       instagram,
+      fechaNacimiento,
     } = parsedData;
 
     if (!username || !password) {
@@ -110,6 +115,7 @@ export async function POST(req: NextRequest, res: NextResponse) {
         genero,
         mail,
         instagram,
+        fechaNacimiento,
         etiquetas: {
           disconnect: {
             id: etiquetaTentativaId.id,
@@ -128,6 +134,7 @@ export async function POST(req: NextRequest, res: NextResponse) {
         genero,
         mail,
         instagram,
+        fechaNacimiento,
         etiquetas: {
           connect: {
             id: modeloEtiquetaId.id,
