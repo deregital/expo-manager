0f25c594c448de498fa765b7139f30b794760e93
Alan Babor <baboralanoroynegro@gmail.com>
2024-08-20 17:04:55 -0300

    add: page de Papelera

---
+++ src/app/(dashboard)/papelera/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+import React from 'react';
+const PapeleraPage = () => {
+  return (
+    <div className='flex flex-col gap-y-5 p-3 md:p-5 '>
+      <p className='text-xl font-bold md:text-3xl'>Papelera</p>
+    </div>
+  );
+};
+
+export default PapeleraPage;
