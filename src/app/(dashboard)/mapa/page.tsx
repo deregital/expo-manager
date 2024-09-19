9a517881017ab6f6053dac48b326d9e0d45d08bc
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2024-09-19 13:20:43 -0300

    add: React Leaflet

--- src/app/(dashboard)/mapa/page.tsx
+++ src/app/(dashboard)/mapa/page.tsx
@@ -1,7 +1,20 @@
+'use client';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+
 const MapaPage = () => {
   return (
     <div>
-      <h1>Mapa</h1>
+      <MapContainer center={[-20, -20]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        />
+        <Marker position={[-20, -20]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 };
