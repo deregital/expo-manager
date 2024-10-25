b9784043548b36d58a1e316086e9a406bfd63a03
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2024-10-25 16:33:23 -0300

    feat: comment router started

---
+++ src/server/routers/commentRouter.ts
@@ -0,0 +1,65 @@
+import { handleError, protectedProcedure, router } from '@/server/trpc';
+import {
+  accountSchema,
+  commentSchema,
+  createCommentSchema,
+} from 'expo-backend-types';
+import { z } from 'zod';
+
+export const commentRouter = router({
+  create: protectedProcedure
+    .input(createCommentSchema)
+    .mutation(async ({ input, ctx }) => {
+      const { data, error } = await ctx.fetch.POST('/comment/create', {
+        body: input,
+      });
+      if (error) {
+        throw handleError(error);
+      }
+      return data;
+    }),
+  getById: protectedProcedure
+    .input(commentSchema.shape.id)
+    .query(async ({ input, ctx }) => {
+      const { data, error } = await ctx.fetch.GET(
+        '/comment/get-by-profile/{id}',
+        {
+          params: {
+            path: {
+              id: input,
+            },
+          },
+        }
+      );
+      if (error) {
+        throw handleError(error);
+      }
+
+      return data;
+    }),
+  toggleSolve: protectedProcedure
+    .input(
+      z.object({
+        id: commentSchema.shape.id,
+        account: accountSchema.omit({ password: true }),
+      })
+    )
+    .mutation(async ({ input, ctx }) => {
+      const { data, error } = await ctx.fetch.PATCH(
+        '/comment/toggle-solve/{id}',
+        {
+          params: {
+            path: {
+              id: input.id,
+            },
+          },
+        }
+      );
+
+      if (error) {
+        throw handleError(error);
+      }
+
+      return data;
+    }),
+});
