4d03338b563e6ade5fcab79c5b0082c94cc003a1
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2024-10-29 16:09:43 -0300

    feat: cannedResponse router updated

---
+++ src/server/routers/canned-responseRouter.ts
@@ -0,0 +1,73 @@
+import { z } from 'zod';
+import { router, protectedProcedure, handleError } from '../trpc';
+import {
+  cannedResponseSchema,
+  createCannedResponseSchema,
+  updateCannedResponseSchema,
+} from 'expo-backend-types';
+
+export const cannedResponseRouter = router({
+  create: protectedProcedure
+    .input(createCannedResponseSchema)
+    .mutation(async ({ input, ctx }) => {
+      const { data, error } = await ctx.fetch.POST('/cannedresponse/create', {
+        body: input,
+      });
+      if (error) {
+        throw handleError(error);
+      }
+      return data;
+    }),
+
+  update: protectedProcedure
+    .input(
+      updateCannedResponseSchema.merge(
+        z.object({ id: cannedResponseSchema.shape.id })
+      )
+    )
+    .mutation(async ({ input, ctx }) => {
+      const { id, ...body } = input;
+      const { data, error } = await ctx.fetch.PATCH(
+        '/cannedresponse/update/{id}',
+        {
+          params: {
+            path: {
+              id: id,
+            },
+          },
+          body: body,
+        }
+      );
+      if (error) {
+        throw handleError(error);
+      }
+      return data;
+    }),
+
+  delete: protectedProcedure
+    .input(cannedResponseSchema.shape.id)
+    .mutation(async ({ input, ctx }) => {
+      const { data, error } = await ctx.fetch.DELETE(
+        '/cannedresponse/delete/{id}',
+        {
+          params: {
+            path: {
+              id: input,
+            },
+          },
+        }
+      );
+      if (error) {
+        throw handleError(error);
+      }
+      return data;
+    }),
+
+  getAll: protectedProcedure.query(async ({ ctx }) => {
+    const { data, error } = await ctx.fetch.GET('/cannedresponse/get-all');
+    if (error) {
+      throw handleError(error);
+    }
+    return data;
+  }),
+});
