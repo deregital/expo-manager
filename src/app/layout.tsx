9ac1235aacbb5b3c041f87d1ff6eeb6f62d78d64
nicofishman <74215078+nicofishman@users.noreply.github.com.>
2025-04-02 23:02:00 -0300

    add: tabs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import TRPCProvider from '@/app/_trpc/Provider';
 import { cn } from '@/lib/utils';
 import NextAuthProvider from '@/app/_auth/NextAuthProvider';
+import { NuqsAdapter } from 'nuqs/adapters/next/app';
 
 const fontSans = Inter({
   subsets: ['latin'],
@@ -29,7 +30,9 @@ const RootLayout = ({
         )}
       >
         <NextAuthProvider>
-          <TRPCProvider>{children}</TRPCProvider>
+          <TRPCProvider>
+            <NuqsAdapter>{children}</NuqsAdapter>
+          </TRPCProvider>
         </NextAuthProvider>
       </body>
     </html>
