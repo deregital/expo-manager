10389b7910f00366f7a2eecda1a57620b5b6cb59
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2025-04-28 23:30:12 -0300

    feat: image routes

---
+++ src/app/api/image/event-picture/route.ts
@@ -0,0 +1,51 @@
+import { type NextRequest, NextResponse } from 'next/server';
+import { fetchClient } from '@/server/fetchClient';
+
+export async function POST(req: NextRequest, res: NextResponse) {
+  const form = await req.formData();
+  const filepath = form.get('imagen') as File;
+  const id = form.get('id') as string;
+
+  if (!filepath)
+    return new NextResponse('No se subió ningún archivo', { status: 400 });
+
+  const formData = new FormData();
+  formData.append('image', filepath, filepath.name);
+
+  const { error } = await fetchClient.PATCH('/event/update-main-picture/{id}', {
+    params: {
+      path: {
+        id,
+      },
+    },
+    // @ts-ignore
+    body: formData,
+  });
+
+  if (error) {
+    return new NextResponse('Error al subir la imagen', { status: 500 });
+  }
+
+  return new NextResponse('Imágen subida con éxito', { status: 200 });
+}
+
+export async function DELETE(req: NextRequest, res: NextResponse) {
+  const form = await req.formData();
+  const id = form.get('id') as string;
+  const { response } = await fetchClient.DELETE(
+    '/event/delete-main-picture/{id}',
+    {
+      params: {
+        path: {
+          id,
+        },
+      },
+    }
+  );
+
+  if (response.ok) {
+    return new NextResponse('Archivo eliminado con éxito', { status: 200 });
+  }
+
+  return new NextResponse('No se mandó ningún archivo', { status: 400 });
+}
