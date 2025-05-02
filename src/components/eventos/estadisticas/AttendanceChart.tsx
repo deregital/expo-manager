7f613f98f7fa45c639c1f6b25cb280b04473c079
Sebastian Biorci <sebipochi@gmail.com>
2025-05-02 02:17:06 -0300

    feat: add functional chart

--- src/components/eventos/estadisticas/AttendanceChart.tsx
+++ src/components/eventos/estadisticas/AttendanceChart.tsx
@@ -16,70 +16,80 @@ import {
   ChartTooltip,
   ChartTooltipContent,
 } from '@/components/ui/chart';
-const chartData = [
-  { month: 'Staff', desktop: 186, mobile: 80 },
-  { month: 'Participant', desktop: 305, mobile: 200 },
-  { month: 'Spectator', desktop: 237, mobile: 120 },
-];
 
 const chartConfig = {
-  desktop: {
-    label: 'Desktop',
+  amount: {
+    label: 'Tickets',
     color: 'hsl(var(--chart-1))',
-  },
-  mobile: {
-    label: 'Mobile',
-    color: 'hsl(var(--chart-2))',
   },
   label: {
     color: 'hsl(var(--background))',
   },
 } satisfies ChartConfig;
 
-export const AttendanceChart = () => {
+interface AttendanceChartProps {
+  data?: Record<string, number | undefined>;
+}
+
+export const AttendanceChart = ({ data }: AttendanceChartProps) => {
+  if (!data) {
+    return (
+      <Card className='flex h-full items-center justify-center'>
+        <CardContent>No hay datos disponibles de tickets emitidos</CardContent>
+      </Card>
+    );
+  }
+  const chartData = Object.entries(data).map(([type, count]) => ({
+    type,
+    count,
+  }));
+
   return (
     <>
       <Card>
-        <CardHeader>
+        <CardHeader className='mb-0 pb-0'>
           <CardTitle>Asistencia Total</CardTitle>
         </CardHeader>
         <CardContent>
-          <ChartContainer className='h-36 w-full' config={chartConfig}>
+          <ChartContainer className='h-36 w-full ' config={chartConfig}>
             <BarChart
               accessibilityLayer
               data={chartData}
               layout='vertical'
-              barSize={32}
+              barSize={26}
+              margin={{
+                right: 24,
+              }}
             >
               <CartesianGrid horizontal={false} />
               <YAxis
-                dataKey='month'
+                dataKey='type'
                 type='category'
                 tickLine={false}
                 axisLine={false}
                 tickFormatter={(value) => value.slice(0, 3)}
                 hide
               />
-              <XAxis dataKey='desktop' type='number' hide />
+              <XAxis dataKey='count' type='number' hide />
               <ChartTooltip
                 cursor={false}
                 content={<ChartTooltipContent indicator='line' />}
               />
               <Bar
-                dataKey='desktop'
+                dataKey='count'
                 layout='vertical'
                 fill='var(--color-desktop)'
                 radius={4}
               >
                 <LabelList
-                  dataKey='month'
+                  dataKey='type'
                   position='insideLeft'
                   offset={8}
                   className='fill-[--color-label]'
                   fontSize={12}
                 />
                 <LabelList
-                  dataKey='desktop'
+                  dataKey='count'
                   position='right'
                   offset={8}
                   className='fill-foreground'
